7846a3f2626b212309a9699ae0606e9e00ab5548
dogao <doga.ozsoyler@gmail.com>
2021-03-01 21:34:42 +0300

    Add game over screen

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -6,15 +6,30 @@ public class GameManager : MonoBehaviour
 {
     SpawnerScript spawnerScript;
     ScoreScript scoreScript;
+    GameOverScript GameOverScript;
+    public GameObject gameOverScreen;
+
+    private void Awake() {
+        scoreScript = FindObjectOfType<ScoreScript>(); // to increaseScore and to get score when finish game
+        gameOverScreen.SetActive(false);
+    }
+    
     public void StopGame()
     {
         spawnerScript = GameObject.Find("Spawner").GetComponent<SpawnerScript>();
         spawnerScript.stop = true;
+        GameOver();
     }
 
     public void IncreaseScore()
     {
-        scoreScript = GameObject.Find("ScoreText").GetComponent<ScoreScript>();
         scoreScript.duckNum++;
+    }
+
+    public void GameOver()
+    {
+        gameOverScreen.SetActive(true);
+        GameOverScript = gameOverScreen.GetComponent<GameOverScript>();
+        GameOverScript.Setup(scoreScript.duckNum);
     }
 }
---
+++ Assets/Script/GameOverScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverScript : MonoBehaviour
+{
+    public Text scoreText;
+    public void Setup(int score)
+    {
+        gameObject.SetActive(true);
+        scoreText.text = "Score " + score.ToString();
+    }
+}
